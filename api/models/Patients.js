7f7e09fd690478085e3c3e611f12ff971322dcfc
madhubala g <madhubala.g@cognizant.com>
2019-03-26 18:36:00 +0530

    minor changes in patients schema

--- api/models/Patients.js
+++ api/models/Patients.js
@@ -4,18 +4,18 @@ const Schema = mongoose.Schema;
 // Create Schema
 const PatientSchema = new Schema({
   // TODO: Create models here...
-  firstName: { type: String, required: true, min: 3, max: 24 },
-  lastName: { type: String, required: true, min: 3, max: 24 },
+  firstName: { type: String, required: true, minlength: 3, maxlength: 24 },
+  lastName: { type: String, required: true, minlength: 3, maxlength: 24 },
   phone: { type: String, pattern: "^([0-9]{3}-[0-9]{3}-[0-9]{4}$", required: true, max: 10 },
   address: { type: String, required: true, max: 1000 },
-  consultedBy: { type: String, required: true, min: 3, max: 24 },
+  consultedBy: { type: String, required: true, minlength: 3, maxlength: 24 },
   age: { type: Number, required: true, min: 0, max: 100 },
   gender: { type: String, required: true },
   consulted: { type: Boolean, default: true },
   created_at: { type: Date, default: Date.now },
-  complaints: { type: String, max: 1000 },
-  Results: { type: String, max: 1000 , default: 'success'},
-  Prescriptions: { type: String, max: 1000 }
+  complaints: { type: String, maxlength: 1000 },
+  Results: { type: String, maxlength: 1000 , default: 'success'},
+  Prescriptions: { type: String, maxlength: 1000 }
 });
 
 module.exports = mongoose.model('Patients', PatientSchema);
